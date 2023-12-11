1e0481f6a7cbfa2920942789641ea40e511fd49a
syedniaz <snm051110@gmail.com>
2023-12-12 01:28:07 +0600

    updated database

--- iub360.sql
+++ iub360.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 08:03 PM
+-- Generation Time: Dec 11, 2023 at 08:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -64,6 +64,28 @@ INSERT INTO `companies` (`id`, `name`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `student_messages`
+--
+
+CREATE TABLE `student_messages` (
+  `message_id` int(6) NOT NULL,
+  `sender_id` int(6) NOT NULL,
+  `receiver_id` int(6) NOT NULL,
+  `messages` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_messages`
+--
+
+INSERT INTO `student_messages` (`message_id`, `sender_id`, `receiver_id`, `messages`) VALUES
+(1, 3, 1, 'Hello, Welcome to IUB 360'),
+(2, 3, 1, 'Nice Work!'),
+(3, 4, 5, 'Good job on the project');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -82,7 +104,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'Syed Niaz', 'syedniaz_10@yahoo.com', '1234', 'student'),
 (2, 'admin1', 'admin1@gmail.com', '1234', 'admin'),
-(3, 'Sabrina Alam', 'sabrina@iub.edu.bd', '1234', 'mentor');
+(3, 'Sabrina Alam', 'sabrina@iub.edu.bd', '1234', 'mentor'),
+(4, 'Subha', 'subha@bolbona.com', '1234', 'mentor'),
+(5, 'Rasheeq Ishmam', 'rasheeq@gmail.com', '1234', 'student');
 
 --
 -- Indexes for dumped tables
@@ -93,6 +117,14 @@ INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `user_type`) VALUES
 --
 ALTER TABLE `companies`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_messages`
+--
+ALTER TABLE `student_messages`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `receiver_id` (`receiver_id`);
 
 --
 -- Indexes for table `users`
@@ -111,10 +143,27 @@ ALTER TABLE `companies`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
+-- AUTO_INCREMENT for table `student_messages`
+--
+ALTER TABLE `student_messages`
+  MODIFY `message_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `student_messages`
+--
+ALTER TABLE `student_messages`
+  ADD CONSTRAINT `student_messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
